067829a716bba70a30ff12addd877029512c751b
nifamilyltd4 <128149925+nifamilyltd4@users.noreply.github.com>
2024-01-17 18:39:34 +0600

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,60 @@
 
+plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.doha.easylink"
+    compileSdk = 33
+    
+    defaultConfig {
+        applicationId = "com.doha.easylink"
+        minSdk = 22
+        targetSdk = 33
+        versionCode = 1
+        versionName = "0.1-alpha"
+        
+        vectorDrawables { 
+            useSupportLibrary = true
+        }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    
+}
+
+dependencies {
+
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-storage")
+   
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
+    
+    implementation("com.squareup.picasso:picasso:2.8")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+
+}
